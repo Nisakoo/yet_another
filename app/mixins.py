dc3942178c1427865ad839a6f3078fb81a1ee47c
Nisakoo <modeniki10@gmail.com>
2025-10-28 23:01:48 +0300

    add pagination and fix some mistakes

---
+++ app/mixins.py
@@ -0,0 +1,30 @@
+from django.views.generic import View
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http.request import HttpRequest
+from django.http.response import HttpResponseNotAllowed
+
+
+class PaginatorMixin:
+    objects_per_page = 10
+    page_number_parameter = 'page'
+
+
+    def dispatch(self, request: 'HttpRequest', *args, **kwargs):
+        if request.method != 'GET':
+            return HttpResponseNotAllowed(['GET'])
+        
+        return super().dispatch(request, *args, **kwargs)
+
+
+    def get_page_object(self: 'View', object_list):
+        paginator = Paginator(object_list, self.objects_per_page)
+        page_number = self.request.GET.get(self.page_number_parameter, 1)
+
+        try:
+            page_object = paginator.page(page_number)
+        except PageNotAnInteger:
+            page_object = paginator.page(1)
+        except EmptyPage:
+            page_object = paginator.page(paginator.num_pages)
+
+        return page_object
--- app/views.py
+++ app/views.py
@@ -2,14 +2,37 @@ import logging
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.views.generic.base import TemplateResponseMixin
+
+from .mixins import PaginatorMixin
 
 
 logger = logging.getLogger(__name__)
 
 
-class HomeView(TemplateView):
+QUESTIONS = [
+    {
+        "id": i,
+        "title": f"Question {i}",
+        "description": "Default description",
+        "answers_count": i * 10 % 9,
+        "tags": ["Rust", "Blazing", "Fast"]
+    } for i in range(30)
+]
+
+
+class HomeView(PaginatorMixin, TemplateView):
+    objects_per_page = 10
     template_name = 'home.html'
+
+    def get_object_list(self):
+        return QUESTIONS
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['page_obj'] = self.get_page_object(self.get_object_list())
+        return context
 
 
 class LoginView(TemplateView):
@@ -24,12 +47,31 @@ class SettingsView(TemplateView):
     template_name = 'settings.html'
 
 
-class HotQuestionsView(TemplateView):
+class HotQuestionsView(PaginatorMixin, TemplateView):
+    objects_per_page = 10
     template_name = 'hot.html'
 
+    def get_object_list(self):
+        return QUESTIONS
 
-class QuestionsByTagView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['page_obj'] = self.get_page_object(self.get_object_list())
+        return context
+
+
+class QuestionsByTagView(PaginatorMixin, TemplateView):
+    objects_per_page = 10
     template_name = 'by_tag.html'
+
+    def get_object_list(self):
+        return QUESTIONS
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['page_obj'] = self.get_page_object(self.get_object_list())
+        context['tag'] = 'Idris'
+        return context
 
 
 class QuestionView(TemplateView):
